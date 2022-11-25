0334bcc54a62be4349e28afec25ab4148ecb7c4f
Aleksandr Vyrodov <aleksandr.vyrodov@hotmail.com>
2022-11-25 11:44:48 +0300

    correct defined

--- local/.mit/.defined.php
+++ local/.mit/.defined.php
@@ -10,15 +10,3 @@ namespace MIT {
 
   const PATH_APP_CORE = PATH_APP . '/.core';
 }
-
-namespace MIT\CATALOG {
-  const PROP_MIN_COUNT_GROUP = 10;
-  const PROP_MAX_COUNT_GROUP = 50;
-
-  const PROP_INF_COUNT_GROUP = 0;
-
-  const PROP_MAIN_CARD = 100;
-  const PROP_INNERCARD_1 = 150;
-  const PROP_INNERCARD_2 = 200;
-  const PROP_INNERCARD_CHARACTER = 1000;
-}
