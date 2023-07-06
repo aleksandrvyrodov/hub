127aae2237d3eebd525e568d9b8e94ed57937edd
Aleksandr <72131704+aleksandrvyrodov@users.noreply.github.com>
2023-07-06 12:33:12 +0300

    [AE] auto_commit and add ignored

---
+++ local/#unsort/auto_commit.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+# crontab -e
+# add line -> @midnight /[path]/auto-commit.py
+
+import os
+import subprocess
+
+def check_repository_status(path):
+    os.chdir(path)
+
+    result = subprocess.run(['git', 'status', '--porcelain'], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, encoding='utf-8')
+
+    if result.returncode != 0:
+        return 2
+
+    output = result.stdout.strip()
+
+    if not output:
+        return 1
+    else:
+        return commit_changes()
+
+
+def commit_changes():
+    result = subprocess.run(['git', 'add', '-A'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    if result.returncode != 0:
+        return 3
+    result = subprocess.run(['git', 'commit', '-m', '<[US-CR|EvD]'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    if result.returncode != 0:
+        return 4
+    return 1
+
+
+if not os.path.exists('/home/m/mtbspb/mtb.spb.ru/public_html/!mit'):
+  result = check_repository_status('/home/m/mtbspb/mtb.spb.ru/public_html')
+  print(result)
+else:
+  print(0)
